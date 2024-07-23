01852e8a4490796107a03d5f4571eb6d31c78cd6
César M. Cristóbal <cesar@callepuzzle.com>
2024-07-23 22:01:56 +0200

    añadimos skeleton y un mini menú

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import '../app.css';
+	import { TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
+</script>
+
+<TabGroup
+	justify="justify-center"
+	active="variant-filled-primary"
+	hover="hover:variant-soft-primary"
+	flex="flex-1 lg:flex-none"
+	rounded=""
+	border=""
+	class="bg-surface-100-800-token w-full"
+>
+	<TabAnchor selected={$page.url.pathname === '/'} href="/">
+		<svelte:fragment slot="lead"><i class="fa-solid fa-book"></i></svelte:fragment>
+		Home
+	</TabAnchor>
+	<TabAnchor selected={$page.url.pathname === '/map'} href="/map/">
+		<svelte:fragment slot="lead"><i class="fa-solid fa-film"></i></svelte:fragment>
+		Mapa
+	</TabAnchor>
+</TabGroup>
+
+<slot />
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const L = (await import('leaflet')).default;
+		const map = L.map('map').setView([41.50878286958457, -4.458162378583092], 17);
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution:
+				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+
+		L.marker([41.507893235550306, -4.461463703773325]).addTo(map).bindPopup('KPY').openPopup();
+	});
+</script>
+
+<div id="map"></div>
+
+<span class="badge-icon variant-filled"> (icon) </span>
+
+<link
+	rel="stylesheet"
+	href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+	integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+	crossorigin=""
+/>
+
+<style>
+	#map {
+		height: 80vh;
+	}
+</style>
