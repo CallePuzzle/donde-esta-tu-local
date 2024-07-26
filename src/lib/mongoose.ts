222cef1ddb5456840c5a43b8079d32dc8cc4c2dc
César M. Cristóbal <cesar@callepuzzle.com>
2024-07-26 21:22:47 +0200

    secrets in .env file

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,13 +1,9 @@
-import { getPlatformProxy } from "wrangler";
+import { MONGO_URI } from '$env/static/private';
 import mongoose from 'mongoose';
 import { logger } from '$lib/server/logger';
 
 // connect to the database
 export async function GetConnectedMongoose(): Promise<typeof mongoose> {
-
-	const { env } = await getPlatformProxy();
-	const MONGO_URI = env.MONGO_URI as string;
-
 	await mongoose.connect(MONGO_URI);
 	mongoose.set('debug', (collectionName, method, query, doc) => {
 		logger.debug({ query: query, doc: doc }, `${collectionName}.${method}`);
