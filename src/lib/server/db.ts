ac55b7e37b4f14089d6edb5db7ba9d3f07b699c2
César M. Cristóbal <cesar@callepuzzle.com>
2024-07-31 18:02:19 +0200

    fix D1 type

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaD1 } from '@prisma/adapter-d1';
 
+import type { D1Database } from '@cloudflare/workers-types';
+
 export function initializePrisma(D1: D1Database) {
 	const adapter = new PrismaD1(D1);
 	return new PrismaClient({ adapter });
