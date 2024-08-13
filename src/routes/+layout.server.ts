55d9a6bf920b8ec4b081e0819f4ebd2b76bc6f16
César M. Cristóbal <cesar@callepuzzle.com>
2024-08-13 14:37:27 +0200

    39 pageserverload refactor (#42)
    * Añadimos mapa antes de validar peña
    
    * sacamos los nombres de los usuarios
    
    * borramos las migraciones, empezamos de 0
    
    * include ambient.d.ts, in tsconfig
    
    * server load refactor

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,46 +1,32 @@
 import { ProtectedRoutes } from '$lib/routes';
-import { JWK } from '$env/static/private';
+import { JWK } from '$env/static/private'; // TODO https://github.com/sveltejs/kit/issues/8882
 import { getPublicKeyFromJwk } from 'cf-webpush';
-import { getUserNotifications } from '$lib/utils/get-user-notifications';
-import { initializePrisma } from '$lib/server/db';
+import { getUserNotifications, type UserNotifications } from '$lib/utils/get-user-notifications';
 
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, PageServerLoadEvent } from './$types';
 
-export const load: PageServerLoad = async (event) => {
-	let user = null;
-	let notifications = null;
-	let notificationsCount = 0;
+export const load: PageServerLoad = async (event: PageServerLoadEvent) => {
+	let userNotification: UserNotifications = {
+		user: null,
+		notifications: [],
+		notificationsCount: 0
+	};
 
 	if (event.locals.user) {
 		const db = event.platform!.env.DB;
-		const prisma = initializePrisma(db);
-		const userNotifications = await getUserNotifications(event.locals.user.id, db);
-		user = userNotifications.user;
-		notifications = userNotifications.notifications;
-		notificationsCount = await prisma.notification.count({
-			where: {
-				users: {
-					some: {
-						id: event.locals.user.id
-					}
-				},
-				status: {
-					not: 'validated'
-				}
-			}
-		});
+		userNotification = await getUserNotifications(event.locals.user.id, db);
 	}
 
 	let path = event.route.id;
 
 	return {
-		userIsLogged: event.locals.user ? true : false,
-		user: user,
 		isProtectedRoute: ProtectedRoutes.some((route) => route.path === path),
 		protectedRouteMessage: ProtectedRoutes.find((route) => route.path === path)?.message,
+		path: path,
+		userIsLogged: event.locals.user ? true : false,
+		user: userNotification.user,
 		JWKpublicKey: getPublicKeyFromJwk(JSON.parse(JWK)),
-		notifications: notifications,
-		notificationsCount: notificationsCount,
-		path: path
+		notifications: userNotification.notifications,
+		notificationsCount: userNotification.notificationsCount
 	};
 };
--- src/routes/notification/my/+page.svelte
+++ src/routes/notification/my/+page.svelte
@@ -1,8 +1,39 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import { onMount } from 'svelte';
+	import { coordsMonte } from '$lib/utils/coords-monte';
 	import { Routes } from '$lib/routes';
 
+	import type { Map } from 'leaflet';
+	import type { PageData } from './$types';
+	import type { Gang, Notification } from '@prisma/client';
+
 	export let data: PageData;
+
+	let currentNotification: Notification;
+	let L: any;
+	let map: Map;
+
+	onMount(async () => {
+		L = (await import('leaflet')).default;
+		map = L.map('map').setView(coordsMonte, 17);
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution:
+				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+	});
+
+	function showModal(notification: Notification): undefined {
+		currentNotification = notification;
+		const gang: Gang = currentNotification.data?.gang;
+
+		map.panTo([gang.latitude, gang.longitude]);
+		L.marker([gang.latitude, gang.longitude]).addTo(map).bindPopup(gang.name);
+
+		const modal = document.getElementById('validate_modal');
+		modal.showModal();
+
+		return undefined;
+	}
 </script>
 
 <div class="flex flex-col">
@@ -23,40 +54,20 @@
 			{#each data.notifications as notification}
 				<li>
 					{#if notification.type === 'gang-added'}
-						<form method="POST" action="{Routes.add_gang.url}?/validate" class="">
-							<span class=""
-								>{JSON.parse(notification.data).addedBy} ha añadido una peña nueva:
-							</span><span class="">{JSON.parse(notification.data).gangName}</span>
-							<label
-								><input
-									type="hidden"
-									class="input w-full max-w-xs"
-									name="userId"
-									value={data.user.id}
-								/></label
+						<span>{notification.data.addedBy.name} ha añadido una peña nueva: </span><span
+							>{notification.data.gang.name}</span
+						>
+						{#if notification.status === 'PENDING'}
+							<button class="btn btn-accent m-6" on:click={showModal(notification)}>Validar</button>
+						{:else if notification.status === 'VALIDATED'}
+							<button class="btn btn-accent m-6" disabled
+								>Validada por {notification.data.reviewedBy?.name}</button
 							>
-							<label
-								><input
-									type="hidden"
-									class="input w-full max-w-xs"
-									name="notificationId"
-									value={notification.id}
-								/></label
+						{:else if notification.status === 'REFUSED'}
+							<button class="btn btn-accent m-6" disabled
+								>Rechazada por {notification.data.reviewedBy?.name}</button
 							>
-							<label
-								><input
-									type="hidden"
-									class="input w-full max-w-xs"
-									name="gangId"
-									value={JSON.parse(notification.data).gangId}
-								/></label
-							>
-							{#if notification.status === 'pending'}
-								<button type="submit" class="btn btn-accent m-6">Validar</button>
-							{:else}
-								<button class="btn btn-accent m-6" disabled>Validada</button>
-							{/if}
-						</form>
+						{/if}
 					{:else}
 						<h2>{notification.title}</h2>
 						<p>{notification.body}</p>
@@ -68,3 +79,84 @@
 		</ul>
 	</div>
 </div>
+
+<dialog id="validate_modal" class="modal">
+	<div class="modal-box">
+		<h3 class="text-lg font-bold">Validar peña {currentNotification?.data?.gang.name}</h3>
+		<div id="map" class="z-0"></div>
+		<p>
+			{currentNotification?.data?.addedBy?.name} ha añadido una peña nueva:
+			<span class="">{currentNotification?.data?.gang.name}</span>
+		</p>
+		<form method="POST" action="{Routes.add_gang.url}?/validate" class="">
+			<label
+				><input
+					type="hidden"
+					class="input w-full max-w-xs"
+					name="userId"
+					value={data.user.id}
+				/></label
+			>
+			<label
+				><input
+					type="hidden"
+					class="input w-full max-w-xs"
+					name="notificationId"
+					value={currentNotification?.id}
+				/></label
+			>
+			<label
+				><input
+					type="hidden"
+					class="input w-full max-w-xs"
+					name="gangId"
+					value={currentNotification?.data?.gangId}
+				/></label
+			>
+			<button type="submit" class="btn btn-accent m-6">Validar</button>
+		</form>
+		<form method="POST" action="{Routes.add_gang.url}?/refuse" class="">
+			<label
+				><input
+					type="hidden"
+					class="input w-full max-w-xs"
+					name="userId"
+					value={data.user.id}
+				/></label
+			>
+			<label
+				><input
+					type="hidden"
+					class="input w-full max-w-xs"
+					name="notificationId"
+					value={currentNotification?.id}
+				/></label
+			>
+			<label
+				><input
+					type="hidden"
+					class="input w-full max-w-xs"
+					name="gangId"
+					value={currentNotification?.data?.gangId}
+				/></label
+			>
+			<button type="submit" class="btn btn-error m-6">Rechazar</button>
+		</form>
+	</div>
+	<form method="dialog" class="modal-backdrop">
+		<button>close</button>
+	</form>
+</dialog>
+
+<link
+	rel="stylesheet"
+	href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+	integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+	crossorigin=""
+/>
+
+<style>
+	#map {
+		height: 40vh;
+	}
+</style>
