73eef51507f8cdc44919bb7026b1cfbf55fbed92
César M. Cristóbal <cesar@callepuzzle.com>
2024-09-04 14:10:11 +0200

    estilos carrousel pasos

--- src/routes/wellcome/step-to-install/+page.svelte
+++ src/routes/wellcome/step-to-install/+page.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { register } from 'swiper/element/bundle';
 	import { Routes } from '$lib/routes';
+	import { register } from 'swiper/element/bundle';
 
 	import step1 from '$lib/assets/step1.png';
 	import step2 from '$lib/assets/step2.png';
 	import step3 from '$lib/assets/step3.png';
 	import step4 from '$lib/assets/step4.png';
-
-	import type { PageData } from './$types';
-	import wellcome from '$lib/stores/wellcome';
-
-	export let data: PageData;
 
 	onMount(async () => {
 		register();
@@ -33,11 +28,20 @@
 			pagination-type="progressbar"
 			navigation="true"
 		>
-			<swiper-slide><img src={step1} alt="step1" /></swiper-slide>
-			<swiper-slide><img src={step2} alt="step2" /></swiper-slide>
-			<swiper-slide><img src={step3} alt="step3" /></swiper-slide>
-			<swiper-slide><img src={step4} alt="step4" /></swiper-slide>
-			<swiper-slide><a href={Routes.wellcome.url}>Volver</a></swiper-slide>
+			<swiper-slide class="flex justify-center"><img src={step1} alt="step1" /></swiper-slide>
+			<swiper-slide class="flex justify-center"><img src={step2} alt="step2" /></swiper-slide>
+			<swiper-slide class="flex justify-center"><img src={step3} alt="step3" /></swiper-slide>
+			<swiper-slide class="flex justify-center"><img src={step4} alt="step4" /></swiper-slide>
+			<swiper-slide class="flex justify-center"
+				><a class="return" href={Routes.wellcome.url}>Volver</a></swiper-slide
+			>
 		</swiper-container>
 	</div>
 </div>
+
+<style>
+	.return {
+		font-size: 1.5rem;
+		margin-top: 10rem;
+	}
+</style>
