eb0f84f70538a722547ab53941e26ea62d6a6c74
César M. Cristóbal <cesar@callepuzzle.com>
2024-09-09 16:45:37 +0200

    fix urlBase64ToUint8Array

--- src/lib/notification/notification-subscribe-user.ts
+++ src/lib/notification/notification-subscribe-user.ts
@@ -35,7 +35,7 @@ function urlBase64ToUint8Array(base64String: string) {
 	const rawData = window.atob(base64);
 	const outputArray = new Uint8Array(rawData.length);
 
-	for (const i = 0; i < rawData.length; ++i) {
+	for (let i = 0; i < rawData.length; ++i) {
 		outputArray[i] = rawData.charCodeAt(i);
 	}
 	return outputArray;
