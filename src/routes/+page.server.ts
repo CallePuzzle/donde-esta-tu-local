e4b5fb8a887378079207227c667d5707b2a0aa71
César M. Cristóbal <cesar@callepuzzle.com>
2025-08-13 13:00:26 +0200

    seed gangs

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,18 @@
 import { logger } from '$lib/logger';
+import prisma from '$lib/server/db';
 import type { PageServerLoad, PageServerLoadEvent } from './$types';
 
 export const load: PageServerLoad = async (event: PageServerLoadEvent) => {
-	logger.debug(event.locals);
+	const gangs = await prisma.gang.findMany({
+		where: {
+			status: {
+				not: 'REFUSED'
+			}
+		}
+	});
+	logger.debug(gangs, 'gangs');
 
-	return {};
+	return {
+		gangs: gangs
+	};
 };
