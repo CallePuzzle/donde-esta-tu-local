f1fd1eefb5a59a0cadf8e13557b3b99503258f82
César M. Cristóbal <cesar@callepuzzle.com>
2025-08-28 11:20:28 +0200

    listado de actividades

--- prisma/seed-activities.ts
+++ prisma/seed-activities.ts
@@ -3,10 +3,6 @@ import { PrismaClient } from '@prisma/client';
 import { SeedActivity, type SeedActivityType } from './seed-activity';
 
 async function SeedActivities(prisma: PrismaClient) {
-	// Clear existing gangs
-	// await prisma.activity.deleteMany();
-	console.log('✅ Cleared existing activities');
-
 	console.log('🎉 Seeding activities...');
 
 	const activities: SeedActivityType[] = [
@@ -14,6 +10,90 @@ async function SeedActivities(prisma: PrismaClient) {
 			name: 'XX Concurso de limonada',
 			date: new Date('2025-09-12T17:30:00.000Z'),
 			desc: 'Salida desde la plaza mayor'
+		},
+		{
+			name: 'VI Babrera Circus Party con Chupitada',
+			date: new Date('2025-09-12T22:30:00.000Z'),
+			placeGangName: 'La Babrera',
+			organisingGangNames: ['La Babrera', 'KPY']
+		},
+		{
+			name: 'Trucha Sound Festival X - Sidrada',
+			date: new Date('2025-09-13T19:00:00.000Z'),
+			dateDesc: 'Después del pregón',
+			placeGangName: 'Los Bugas',
+			organisingGangNames: ['Los Bugas']
+		},
+		{
+			name: 'V Danza del Sapo',
+			date: new Date('2025-09-14T02:00:00.000Z'),
+			dateDesc: 'Después del baile',
+			placeGangName: 'El Desmadre',
+			organisingGangNames: ['El Desmadre']
+		},
+		{
+			name: 'II La Chanidad',
+			date: new Date('2025-09-15T14:00:00.000Z'),
+			placeGangName: 'El K-pote',
+			organisingGangNames: ['El K-pote']
+		},
+		{
+			name: 'Meterla en la viga',
+			date: new Date('2025-09-15T18:00:00.000Z'),
+			dateDesc: 'Después de los toros',
+			placeGangName: 'Badulake',
+			organisingGangNames: ['Badulake']
+		},
+		{
+			name: 'El Corzo',
+			date: new Date('2025-09-15T21:59:00.000Z'),
+			placeGangName: 'La movida',
+			organisingGangNames: ['La movida']
+		},
+		{
+			name: 'Juego el Pañuelo',
+			date: new Date('2025-09-16T1:00:00.000Z'),
+			dateDesc: 'Descanso del baile',
+			placeGangName: '13 la víspera',
+			organisingGangNames: ['13 la víspera']
+		},
+		{
+			name: 'Viva el Vino Uve Palito',
+			date: new Date('2025-09-16T18:00:00.000Z'),
+			dateDesc: 'Después de los toros',
+			placeGangName: 'Talanquera',
+			organisingGangNames: ['Talanquera']
+		},
+		{
+			name: 'III Bingo bingo',
+			date: new Date('2025-09-17T1:00:00.000Z'),
+			dateDesc: 'Descanso del baile',
+			desc: 'En el baile',
+			organisingGangNames: ['La Babrera']
+		},
+		{
+			name: 'II El Komplote',
+			date: new Date('2025-09-17T02:00:00.000Z'),
+			dateDesc: 'Después del baile',
+			placeGangName: 'El K-pote',
+			organisingGangNames: ['El K-pote']
+		},
+		{
+			name: 'Charanga La Resaka',
+			date: new Date('2025-09-17T18:00:00.000Z'),
+			dateDesc: 'Después de los toros',
+			placeGangName: 'Las druidas'
+		},
+		{
+			name: 'Vermut la Bodega',
+			date: new Date('2025-09-18T11:30:00.000Z'),
+			desc: 'Raúl'
+		},
+		{
+			name: 'Tardeo',
+			date: new Date('2025-09-18T14:30:00.000Z'),
+			placeGangName: 'Las Nomadas',
+			organisingGangNames: ['Las Nomadas']
 		}
 	];
 
--- prisma/seed-activity.ts
+++ prisma/seed-activity.ts
@@ -1,10 +1,11 @@
-import type { Activity, PrismaClient } from '@prisma/client';
+import type { PrismaClient, Prisma } from '@prisma/client';
 
 export type SeedActivityType = {
 	name: string;
 	date: Date;
+	dateDesc?: string;
 	desc?: string;
-	organisingGangNames: string[];
+	organisingGangNames?: string[];
 	placeGangName?: string;
 };
 
@@ -13,13 +14,14 @@ export async function SeedActivity(prisma: PrismaClient, activity: SeedActivityT
 		id: number;
 	};
 
-	const { name, date, desc, organisingGangNames, placeGangName } = activity;
+	const { name, date, desc: descInput, organisingGangNames, placeGangName } = activity;
+	const desc = descInput ?? null;
 
 	const organisingGangs: OrganisingGang[] = [];
 
 	if (organisingGangNames) {
 		for (let i = 0; i < organisingGangNames.length; i++) {
-			const gang = await prisma.gang.findUnique({
+			const gang = await prisma.gang.findFirst({
 				where: {
 					name: organisingGangNames[i]
 				}
@@ -32,10 +34,11 @@ export async function SeedActivity(prisma: PrismaClient, activity: SeedActivityT
 		}
 	}
 
-	let activitySeeded: Activity;
+	let update: Prisma.ActivityUpdateInput;
+	let create: Prisma.ActivityCreateInput;
 
 	if (placeGangName) {
-		const placeGang = await prisma.gang.findUnique({
+		const placeGang = await prisma.gang.findFirst({
 			where: {
 				name: placeGangName
 			}
@@ -44,30 +47,50 @@ export async function SeedActivity(prisma: PrismaClient, activity: SeedActivityT
 			console.log('⚠️  No se encontró la peña ' + placeGangName);
 			return;
 		}
+		update = {
+			name,
+			desc,
+			organisingGangs: {
+				connect: organisingGangs
+			},
+			placeGang: { connect: { id: placeGang.id } }
+		};
 
-		activitySeeded = await prisma.activity.create({
-			data: {
-				name,
-				date,
-				desc,
-				organisingGangs: {
-					connect: organisingGangs
-				},
-				placeGang: { connect: { id: placeGang.id } }
-			}
-		});
+		create = {
+			name,
+			date,
+			desc,
+			organisingGangs: {
+				connect: organisingGangs
+			},
+			placeGang: { connect: { id: placeGang.id } }
+		};
 	} else {
-		activitySeeded = await prisma.activity.create({
-			data: {
-				name,
-				date,
-				desc,
-				organisingGangs: {
-					connect: organisingGangs
-				}
+		update = {
+			name,
+			desc,
+			organisingGangs: {
+				connect: organisingGangs
 			}
-		});
+		};
+
+		create = {
+			name,
+			date,
+			desc,
+			organisingGangs: {
+				connect: organisingGangs
+			}
+		};
 	}
+
+	const activitySeeded = await prisma.activity.upsert({
+		where: {
+			date
+		},
+		update,
+		create
+	});
 
 	console.log('✅  Actividad creada: ' + activitySeeded.name);
 }
