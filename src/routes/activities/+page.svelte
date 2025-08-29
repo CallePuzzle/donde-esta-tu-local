7282e3eec24594fc22c6256188b26bc0f1d87731
César M. Cristóbal <cesar@callepuzzle.com>
2025-08-29 20:34:38 +0200

    fix activity card, peña link

--- src/routes/activities/+page.svelte
+++ src/routes/activities/+page.svelte
@@ -48,8 +48,21 @@
 					{/each}
 				</div>
 			{:else}
-				<div class="mb-8 rounded-lg border border-blue-200 bg-blue-50 p-6">
-					<p class="text-blue-800">No hay actividades próximas programadas.</p>
+				<div role="alert" class="alert alert-warning">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 w-6 shrink-0 stroke-current"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+						/>
+					</svg>
+					<span>No hay actividades próximas</span>
 				</div>
 			{/if}
 		</div>
@@ -68,8 +81,21 @@
 					{/each}
 				</div>
 			{:else}
-				<div class="mb-8 rounded-lg border border-gray-200 bg-gray-50 p-6">
-					<p class="text-gray-600">No hay actividades pasadas registradas.</p>
+				<div role="alert" class="alert alert-warning">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 w-6 shrink-0 stroke-current"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+						/>
+					</svg>
+					<span>No hay actividades pasadas</span>
 				</div>
 			{/if}
 		</div>
