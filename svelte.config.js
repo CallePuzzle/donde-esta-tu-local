0cf9ab4f461c0b6543c7883fa0376b1769d84ca3
César M. Cristóbal <cesar@callepuzzle.com>
2025-08-30 16:32:25 +0200

    add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			images: {
+				sizes: [640, 828, 1200, 1920, 3840],
+				formats: ['image/avif', 'image/webp'],
+				minimumCacheTTL: 300,
+				domains: [
+					'donde-esta-tu-local-git-staging-jilgues-projects.vercel.app',
+					'donde-esta-tu-local.vercel.app',
+					'xn--peas-hqa.montemayordepililla.cc'
+				]
+			}
+		}),
 		experimental: {
 			tracing: {
 				server: true
