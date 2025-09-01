dc3a7ad98b6368ef028163a86a0fb161d9c91e82
César M. Cristóbal <cesar@callepuzzle.com>
2025-09-01 23:14:15 +0200

    127 guardar envios en db para no mandar el correo dos veces (#130)
    * add email sent model
    
    * wip sent email
    
    * fix login check email sent
    
    * remove database

--- src/lib/components/FormLogin.svelte
+++ src/lib/components/FormLogin.svelte
@@ -42,6 +42,19 @@
 			try {
 				message = null;
 
+				const checkEmailSent = await fetch(
+					`/api/user/email-sent-check?email=${encodeURIComponent($formData.email)}`
+				);
+				const checkResult = await checkEmailSent.json();
+
+				if (!checkResult?.canSend) {
+					sending = false;
+					message = {
+						type: 'error',
+						text: checkResult.error || 'Cannot send opt email code at this time'
+					};
+					return;
+				}
 				const { data, error } = await authClient.emailOtp.sendVerificationOtp({
 					email: $formData.email,
 					type: 'sign-in'
